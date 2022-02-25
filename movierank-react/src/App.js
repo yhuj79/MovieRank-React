9d0c3b4eaff7a2ab2b6bb369157fdfd01e025145
yhuj79 <yu1085@naver.com>
2022-02-26 02:19:46 +0900

    22.02.26.commit.

--- movierank-react/src/App.js
+++ movierank-react/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import YearTab from './Components/YearTab';
+import Home from './Components/Home';
+import Test2021 from './Components/test2021';
+import Test2020 from './Components/test2020';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <YearTab />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/2021" element={<Test2021 />} />
+          <Route path="/2020" element={<Test2020 />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
-
 export default App;
